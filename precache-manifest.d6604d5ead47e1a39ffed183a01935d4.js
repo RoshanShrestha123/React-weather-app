839aed4b1c6dea4c26cff76a8be31f83d3a9b0c9
roshan shrestha <roshan.shrestha4u@gmail.com>
2020-01-09 09:23:28 +0545

    Updates

---
+++ precache-manifest.d6604d5ead47e1a39ffed183a01935d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9169d824025b78702345b26acede84d",
+    "url": "/React-weather-app/index.html"
+  },
+  {
+    "revision": "7fd2e93b5f3975c1f9c7",
+    "url": "/React-weather-app/static/css/main.41c1505d.chunk.css"
+  },
+  {
+    "revision": "155be99d81a9439c74eb",
+    "url": "/React-weather-app/static/js/2.7f91ff56.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-weather-app/static/js/2.7f91ff56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fd2e93b5f3975c1f9c7",
+    "url": "/React-weather-app/static/js/main.f071d02f.chunk.js"
+  },
+  {
+    "revision": "778e3dc846acaac7e1d5",
+    "url": "/React-weather-app/static/js/runtime-main.b0ae7f4c.js"
+  }
+]);
